146017e7dd83f7192ed88777924f9dd72c9d48c7
Muhammad Azril <mhmdazril1724@gmail.com>
2025-03-15 20:50:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -12,13 +12,11 @@ import (
 )
 
 func main() {
-	
 	endpoint := "203.194.113.6:9000"
 	accessKeyID := "MidFtK0wfiZ6AUjDfZbz"
 	secretAccessKey := "KxkgFNq196ok2AKq9U5h2naOUq0Akpi8HyjA4RO3"
 	useSSL := false
 
-	
 	minioClient, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
 		Secure: useSSL,
@@ -28,36 +26,34 @@ func main() {
 	}
 
 	bucketName := "smk-telkom"
-	objectName := "public/test.txt" 
+	objectName := "public/test.txt"
 	filePath := "./test.txt"
+	downloadedFilePath := "downloaded_" + filePath
 
 	fmt.Println("\n=== Upload File ===")
 	content := []byte("Ini adalah contoh file txt yang akan diupload ke MinIO")
 	err = os.WriteFile(filePath, content, 0644)
 	if err != nil {
-		log.Printf("Error membuat file: %v\n", err)
+		log.Fatalf("Error membuat file: %v\n", err)
 	}
 
 	info, err := minioClient.FPutObject(context.Background(), bucketName, objectName, filePath,
 		minio.PutObjectOptions{ContentType: "text/plain"})
 	if err != nil {
-		log.Printf("Error upload file: %v\n", err)
+		log.Fatalf("Error upload file: %v\n", err)
 	} else {
 		fmt.Printf("Berhasil upload %s dengan ukuran %d bytes\n", objectName, info.Size)
 	}
 
 	fmt.Println("\n=== Download File ===")
-	// Download file
-	err = minioClient.FGetObject(context.Background(), bucketName, objectName, "downloaded_"+filePath,
+	err = minioClient.FGetObject(context.Background(), bucketName, objectName, downloadedFilePath,
 		minio.GetObjectOptions{})
 	if err != nil {
-		log.Printf("Error download file: %v\n", err)
+		log.Fatalf("Error download file: %v\n", err)
 	} else {
-		fmt.Printf("Berhasil download file ke downloaded_%s\n", filePath)
+		fmt.Printf("Berhasil download file ke %s\n", downloadedFilePath)
 	}
-
 	fmt.Println("\n=== Daftar File dalam Bucket ===")
-	// List files
 	objectCh := minioClient.ListObjects(context.Background(), bucketName, minio.ListObjectsOptions{
 		Recursive: true,
 	})
@@ -70,19 +66,27 @@ func main() {
 	}
 
 	fmt.Println("\n=== Generate URL ===")
-	// Generate presigned URL
 	presignedURL, err := minioClient.PresignedGetObject(context.Background(), bucketName, objectName, time.Hour*24, nil)
 	if err != nil {
-		log.Printf("Error membuat presigned URL: %v\n", err)
+		log.Fatalf("Error membuat presigned URL: %v\n", err)
 	} else {
 		fmt.Printf("Presigned URL (berlaku 24 jam): %s\n", presignedURL.String())
 	}
 
 	publicURL := fmt.Sprintf("http://%s/%s/%s", endpoint, bucketName, objectName)
 	fmt.Printf("Public URL: %s\n", publicURL)
-	
-	os.Remove(filePath)
-	os.Remove("downloaded_" + filePath)
+
+	var pilihan string
+	fmt.Print("\nApakah Anda ingin menghapus file lokal? (y/n): ")
+	fmt.Scanln(&pilihan)
+
+	if pilihan == "y" || pilihan == "Y" {
+		os.Remove(filePath)
+		os.Remove(downloadedFilePath)
+		fmt.Println("File telah dihapus.")
+	} else {
+		fmt.Println("File tidak dihapus.")
+	}
 
 	fmt.Println("\nProgram selesai")
 }
